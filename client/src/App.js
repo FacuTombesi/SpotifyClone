941222319051516285748e7434ed5a332809190b
FacuTombesi <facundomtombesi@gmail.com>
2024-03-19 13:56:44 -0300

    added bootstrap & basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css"
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 const code = new URLSearchParams(window.location.search).get("code")
 
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,16 @@
+import { Container } from "react-bootstrap"
+
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=1a4f57adf26642ab827be0fe7d11810d&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
   return (
-    <>
-      <a href={AUTH_URL} >
-        <button>Login with Spotify</button>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL} >
+        Login with Spotify
       </a>
-    </>
+    </Container>
   )
 }
