32243b31998a61830293dd6920c4a5c925eaa19a
FacuTombesi <facundomtombesi@gmail.com>
2024-03-19 13:55:31 -0300

    client accessToken & refreshToken fix

--- client/src/utils/useAuth.js
+++ client/src/utils/useAuth.js
@@ -12,7 +12,35 @@ export default function useAuth(code) {
         code
       })
       .then(res => {
-        console.log(res.data)
+        setAccessToken(res.data.accessToken)
+        setRefreshToken(res.data.refreshToken)
+        setExpiresIn(res.data.expiresIn)
+        window.history.pushState({}, null, "/")
+      })
+      .catch(() => {
+        // window.location = "/"
       })
   }, [code])
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return
+
+    const timeout = setTimeout(()=> {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken
+        })
+        .then(res => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch(() => {
+          window.location = "/"
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearTimeout(timeout)
+  }, [refreshToken, expiresIn])
+
+  return accessToken
 }
