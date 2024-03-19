bc53c960ce85a62a532905f9bdf036609d63efb7
FacuTombesi <facundomtombesi@gmail.com>
2024-03-19 13:54:51 -0300

    server accessToken & refreshToken fix

--- server/server.js
+++ server/server.js
@@ -8,6 +8,27 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri:  "http://localhost:3000",
+    clientId: "1a4f57adf26642ab827be0fe7d11810d",
+    clientSecret: "6cad8dabb3a649aa91030ef34b88ad20",
+    refreshToken
+  })
+
+  spotifyApi.refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn
+      })
+    })
+    .catch(() => {
+      res.sendStatus(400)
+    })
+})
+
 app.post("/login", (req, res) => {
   const code = req.body.code
   const spotifyApi = new SpotifyWebApi({
@@ -25,7 +46,6 @@ app.post("/login", (req, res) => {
       })
     })
     .catch(error => {
-      console.log(error)
       res.sendStatus(400)
     })
 })
